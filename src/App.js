19834793c9dfa6178be6018a2471934290accdcd
rhinoxD <coolshiv0721@gmail.com>
2022-01-12 19:02:41 +0530

    Basic Shop Page created and shop-data added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 import './App.css';
-
-const HatsPage = () => {
-  return <h1>Hats</h1>;
-};
 
 function App() {
   return (
     <div>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop/hats' component={HatsPage} />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
