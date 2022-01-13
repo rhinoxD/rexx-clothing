11edec01af85591f689ae96bce449704d4c46b8d
rhinoxD <coolshiv0721@gmail.com>
2022-01-13 10:03:52 +0530

    Sign In Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import './App.css';
+
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
-import './App.css';
 import Header from './components/header/Header';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Header />
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={ShopPage} />
+      <Route exact path='/signin' component={SignInAndSignUpPage} />
     </div>
   );
 }
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/SignIn';
+
+import './sign-in-and-sign-up.scss';
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;
