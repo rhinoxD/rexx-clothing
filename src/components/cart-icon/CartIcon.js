433cb391f81f1a07492640fce79d16bc232bd5f2
rhinoxD <coolshiv0721@gmail.com>
2022-01-13 19:45:31 +0530

    Cart Component

---
+++ src/components/cart-icon/CartIcon.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+import './CartIcon.scss';
+
+const CartIcon = () => {
+  return (
+    <div className='cart-icon'>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>0</span>
+    </div>
+  );
+};
+
+export default CartIcon;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/CartIcon';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -30,6 +31,7 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
     </div>
   );
