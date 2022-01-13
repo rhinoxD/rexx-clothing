090be0628f6fc941589fab52ada6df65c9ad5eea
rhinoxD <coolshiv0721@gmail.com>
2022-01-13 14:34:43 +0530

    Implemented firebase utisl, included ability to store authenticated users

--- src/components/sign-up/SignUp.js
+++ src/components/sign-up/SignUp.js
@@ -7,7 +7,7 @@ import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
 import './SignUp.scss';
 
-class SignUp {
+class SignUp extends React.Component {
   constructor() {
     super();
     this.state = {
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/SignIn';
+import SignUp from '../../components/sign-up/SignUp';
 
 import './sign-in-and-sign-up.scss';
 
@@ -8,6 +9,7 @@ const SignInAndSignUpPage = () => {
   return (
     <div className='sign-in-and-sign-up'>
       <SignIn />
+      <SignUp />
     </div>
   );
 };
