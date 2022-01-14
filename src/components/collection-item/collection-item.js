91f9eaef2b4e6f318072cd5f1e7a0446ecf7b03c
rhinoxD <coolshiv0721@gmail.com>
2022-01-14 10:18:10 +0530

    Add To Cart Styling

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CustomButton from '../custom-button/CustomButton';
 
 import './collection-item.scss';
 
@@ -10,6 +11,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
+      <CustomButton inverted>Add to cart</CustomButton>
     </div>
   );
 };
