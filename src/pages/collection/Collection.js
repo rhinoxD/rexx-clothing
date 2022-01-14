ee390876aeab887aca786b20c46a0d3edcfd158a
rhinoxD <coolshiv0721@gmail.com>
2022-01-14 19:40:21 +0530

    Collection Routing and Selector

--- src/pages/collection/Collection.js
+++ src/pages/collection/Collection.js
@@ -1,3 +1,7 @@
+import { connect } from 'react-redux';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
 import './Collection.scss';
 
 const CollectionPage = ({ match }) => {
@@ -8,4 +12,8 @@ const CollectionPage = ({ match }) => {
   );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
