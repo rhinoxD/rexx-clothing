f4a0b041a0b3e872bfd57e6cab65d70b4b03b18d
rhinoxD <coolshiv0721@gmail.com>
2022-01-15 13:13:47 +0530

    Styled Components added in CartDropdown Component

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -7,29 +7,34 @@ import CartItem from '../cart-item/CartItem';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { toggleCartHidden } from '../../redux/cart/cartActions';
 
-import './CartDropdown.scss';
+import {
+  CartDropdownContainer,
+  EmptyMessageContainer,
+  CartItemsContainer,
+  CartDropdownButton,
+} from './CartDropdown.styles';
 
 const CartDropdown = ({ cartItems, history, dispatch }) => {
   return (
-    <div className='cart-dropdown'>
-      <div className='cart-items'>
+    <CartDropdownContainer>
+      <CartItemsContainer>
         {cartItems.length ? (
           cartItems.map((cartItem) => (
             <CartItem key={cartItem.id} item={cartItem} />
           ))
         ) : (
-          <span className='empty-message'>Your cart is empty</span>
+          <EmptyMessageContainer>Your cart is empty</EmptyMessageContainer>
         )}
-      </div>
-      <CustomButton
+      </CartItemsContainer>
+      <CartDropdownButton
         onClick={() => {
           history.push('/checkout');
           dispatch(toggleCartHidden());
         }}
       >
         GO TO CHECKOUT
-      </CustomButton>
-    </div>
+      </CartDropdownButton>
+    </CartDropdownContainer>
   );
 };
 
---
+++ src/components/cart-dropdown/CartDropdown.styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import CustomButton from '../custom-button/CustomButton';
+
+export const CartDropdownContainer = styled.div`
+  position: absolute;
+  width: 280px;
+  height: 365px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border: 1px solid black;
+  background-color: white;
+  top: 90px;
+  right: 40px;
+  z-index: 5;
+`;
+
+export const EmptyMessageContainer = styled.span`
+  font-size: 18px;
+  margin: 50px auto;
+`;
+
+export const CartItemsContainer = styled.div`
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+`;
+
+export const CartDropdownButton = styled(CustomButton)`
+  margin-top: auto;
+`;
