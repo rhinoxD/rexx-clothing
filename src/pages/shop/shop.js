7bdd7da735c2bff88682b260305cee866584d3e8
rhinoxD <coolshiv0721@gmail.com>
2022-01-15 19:25:47 +0530

    Bringing Shop Data to Our App

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,15 +1,34 @@
+import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
 import CollectionPage from '../collection/Collection';
 
-const ShopPage = ({ match }) => {
-  return (
-    <div className='shop-page'>
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>
-  );
-};
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+  componentDidMount() {
+    const collectionRef = firestore.collection('collections');
+    collectionRef.onSnapshot(async (snapshot) => {
+      convertCollectionsSnapshotToMap(snapshot);
+    });
+  }
+  render() {
+    const { match } = this.props;
+    return (
+      <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
