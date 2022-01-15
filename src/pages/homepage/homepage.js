b86454a5cb176913174ec16e51e0397ce23a200a
rhinoxD <coolshiv0721@gmail.com>
2022-01-15 12:30:08 +0530

    Styled-components added in Header and Homepage

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Directory from '../../components/directory/directory';
 
-import './homepage.scss';
+import { HomePageContainer } from './homepage.styles';
 
 const HomePage = () => {
   return (
-    <div className='homepage'>
+    <HomePageContainer>
       <Directory />
-    </div>
+    </HomePageContainer>
   );
 };
 
