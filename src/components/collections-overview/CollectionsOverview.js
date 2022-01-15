b60a636cb6f992010ae99de7e3ee247016a915fd
rhinoxD <coolshiv0721@gmail.com>
2022-01-15 10:19:33 +0530

    Moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with redux flow for directory/shop

--- src/components/collections-overview/CollectionsOverview.js
+++ src/components/collections-overview/CollectionsOverview.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview';
 
@@ -18,7 +18,7 @@ const CollectionsOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -8,6 +8,11 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
 export const selectCollection = memoize((collectionUrlParam) => {
   return createSelector(
     [selectCollections],
