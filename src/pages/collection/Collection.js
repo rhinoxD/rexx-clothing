4c24e4ba6f88c46f1b49cc82e34be318a8c8fc1b
rhinoxD <coolshiv0721@gmail.com>
2022-01-15 10:06:26 +0530

    Data Normalization + Collection Page

--- src/pages/collection/Collection.js
+++ src/pages/collection/Collection.js
@@ -1,13 +1,21 @@
 import { connect } from 'react-redux';
 
+import CollectionItem from '../../components/collection-item/collection-item';
+
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './Collection.scss';
 
-const CollectionPage = ({ match }) => {
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
   return (
     <div className='collection-page'>
-      <h2>Collection Page</h2>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,14 +1,6 @@
 import { createSelector } from 'reselect';
 import memoize from 'lodash.memoize';
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -17,9 +9,8 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = memoize((collectionUrlParam) => {
-  return createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  return createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   );
 });
