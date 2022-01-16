89a49e2e95d790d66014157794b50c816b76ebc7
rhinoxD <coolshiv0721@gmail.com>
2022-01-16 10:50:11 +0530

    Converting CartItem to Styled Component

--- src/components/cart-item/CartItem.js
+++ src/components/cart-item/CartItem.js
@@ -1,16 +1,20 @@
-import './CartItem.scss';
+import {
+  CartItemContainer,
+  ItemDetailsContainer,
+  CartItemImage,
+} from './CartItem.styles';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
   return (
-    <div className='cart-item'>
-      <img src={imageUrl} alt='item' />
-      <div className='item-details'>
-        <span className='name'>{name}</span>
-        <span className='price'>
+    <CartItemContainer>
+      <CartItemImage src={imageUrl} alt='item' />
+      <ItemDetailsContainer>
+        <span>{name}</span>
+        <span>
           {quantity} x ${price}
         </span>
-      </div>
-    </div>
+      </ItemDetailsContainer>
+    </CartItemContainer>
   );
 };
 
