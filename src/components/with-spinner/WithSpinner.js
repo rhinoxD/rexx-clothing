fd8e54fc08f7c704b0423107248bc1212fbda155
rhinoxD <coolshiv0721@gmail.com>
2022-01-16 09:58:43 +0530

    Added WithSpinner HOC

---
+++ src/components/with-spinner/WithSpinner.js
@@ -0,0 +1,16 @@
+import { SpinnerContainer, SpinnerOverlay } from './WithSpinner.styles';
+
+const WithSpinner = (WrappedComponent) => {
+  const Spinner = ({ isLoading, ...otherProps }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...otherProps} />
+    );
+  };
+  return Spinner;
+};
+
+export default WithSpinner;
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -10,12 +10,12 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  (collections) => Object.keys(collections).map((key) => collections[key])
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
 );
 
 export const selectCollection = memoize((collectionUrlParam) => {
-  return createSelector(
-    [selectCollections],
-    (collections) => collections[collectionUrlParam]
+  return createSelector([selectCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
   );
 });
