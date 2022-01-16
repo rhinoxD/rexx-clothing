126451d67e81e4cde34a59fb367a114714ad6ac8
rhinoxD <coolshiv0721@gmail.com>
2022-01-16 18:22:12 +0530

    Converting SignUp to Styled Component

--- src/components/sign-up/SignUp.js
+++ src/components/sign-up/SignUp.js
@@ -5,7 +5,7 @@ import CustomButton from '../custom-button/CustomButton';
 
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
-import './SignUp.scss';
+import { SignUpContainer, SignUpTitle } from './SignUp.styles';
 
 class SignUp extends React.Component {
   constructor() {
@@ -47,8 +47,8 @@ class SignUp extends React.Component {
   render() {
     const { displayName, email, password, confirmPassword } = this.state;
     return (
-      <div className='sign-up'>
-        <h2 className='title'>I do not have an account</h2>
+      <SignUpContainer>
+        <SignUpTitle>I do not have an account</SignUpTitle>
         <span>Sign up with your email and password</span>
         <form className='sign-up-form' onSubmit={this.handleSubmit}>
           <FormInput
@@ -85,7 +85,7 @@ class SignUp extends React.Component {
           />
           <CustomButton type='submit'>SIGN UP</CustomButton>
         </form>
-      </div>
+      </SignUpContainer>
     );
   }
 }
