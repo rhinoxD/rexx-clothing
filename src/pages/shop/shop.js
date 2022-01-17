328cffedfb87ab03ea4166f14219f9438b363c8f
rhinoxD <coolshiv0721@gmail.com>
2022-01-17 09:40:41 +0530

    Converted Observer pattern to Promise Pattern in Shop page

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -24,7 +24,7 @@ class ShopPage extends React.Component {
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
-    collectionRef.onSnapshot(async (snapshot) => {
+    collectionRef.get().then((snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
