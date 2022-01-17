4f65909825dc7b3bde2e8ccfd1cdfed3ea8bbadd
rhinoxD <coolshiv0721@gmail.com>
2022-01-17 12:23:51 +0530

    Redux Thunk Into Saga

--- src/redux/shop/shopSagas.js
+++ src/redux/shop/shopSagas.js
@@ -1,9 +1,29 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from './shopActions';
 
 import { ShopActionTypes } from './shopTypes';
 
 export function* fetchCollectionsAsync() {
-  yield console.log('I am Fired!');
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
