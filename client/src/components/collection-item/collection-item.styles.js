c9de1b337fefaeb9765ba5117c2ff528847f7e28
rhinoxD <coolshiv0721@gmail.com>
2022-01-21 10:45:21 +0530

    Edited menu-item.styles and collection-item.styles

--- client/src/components/collection-item/collection-item.styles.js
+++ client/src/components/collection-item/collection-item.styles.js
@@ -17,6 +17,17 @@ export const CollectionItemContainer = styled.div`
       display: flex;
     }
   }
+  @media screen and (max-width: 800px) {
+    width: 40vw;
+    &:hover {
+      .image {
+        opacity: unset;
+      }
+      button {
+        opacity: unset;
+      }
+    }
+  }
 `;
 
 export const AddButton = styled(CustomButton)`
