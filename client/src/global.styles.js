255464d122e9e3bf0cfd218b83c6bfa5dde28ecf
rhinoxD <coolshiv0721@gmail.com>
2022-01-21 10:22:19 +0530

    Added first media query

--- client/src/global.styles.js
+++ client/src/global.styles.js
@@ -4,6 +4,9 @@ export const GlobalStyle = createGlobalStyle`
 body {
   font-family: 'Open Sans Condensed';
   padding: 20px 60px;
+  @media screen and (max-width: 800px) {
+    padding: 10px;
+  }
 }
 
 a {
